35ed45efee94442c96b04b46d333deb486d8a2a9
Fabio Zoratti <fabio.zoratti96@gmail.com>
2018-08-20 15:19:31 +0200

    For cycle in roofit_bkg

--- bin/script/roofit_bkg.py
+++ bin/script/roofit_bkg.py
@@ -2,11 +2,10 @@
 # -*- coding:utf-8 -*-
 
 from ROOT import TH1F, TFile, TMath, TF1, TH1F, TCanvas, TGraph, TChain
-from ROOT import RooRealVar, RooGenericPdf, RooArgSet, RooArgList, RooDataSet, RooAbsPdf
+from ROOT import RooRealVar, RooGenericPdf, RooArgSet, RooArgList, RooDataSet, RooAbsPdf, \
+    RooWorkspace
 from ROOT.RooFit import bindFunction, Binning
 
-from math import erf
-from array import array
 from hbbstyle import create_style, init_hist, CMS_prelim
 
 import ROOT
@@ -25,6 +24,10 @@ bbins = [
 ]
 
 
+base_dir = "/nfs/dust/cms/user/zorattif/output"
+input_dir_bkg = os.path.join(base_dir, "raw_files/bkg/no_reranking/medium_wp")
+output_dir_fitted = os.path.join(base_dir, "fit/bkg/no_reranking/medium_wp")
+
 def prendi_parametri(funzione):
     params = list()
     errors = list()
@@ -39,56 +42,64 @@ def scrivi_parametri(params, errors, fileout):
         fileout.write(str(p) + "\t" + str(e) + "\t")
 
 
-
-input_dir_bkg = "/nfs/dust/cms/user/zorattif/output/raw_files/bkg/no_reranking/medium_wp"
-correction_level = ["nothing_true",
-                    "regression_true",
-                    "regression_fsr_true",
-                    "regression_fsr_true",
+correction_level_bkg = [
+    "nothing_false",
+    "regression_false",
+    "fsr_false",
+    "regression_fsr_false",
+    "nothing_true",
+    "regression_true",
+    "fsr_true",
+    "regression_fsr_true",
 ]
+
 lep = [True, False]
-ch = TChain("output_tree")
-ch.Add(os.path.join(input_dir_bkg, "bkg_*_nothing_true.root"))
-tree = ch.CopyTree("Leptonic_event")
+eras = ["C", "D", "E"]
 
-
-Mass = RooRealVar("Mass", "Mass", 120, 600)
-p0 = RooRealVar("p0", "p0", 0, 0.005)
-p1 = RooRealVar("p1", "p1", 1600, 2000)
-p3 = RooRealVar("p3", "p3", 30, 50)
-p4 = RooRealVar("p4", "p4", 40, 80)
-p5 = RooRealVar("p5", "p5", 0, 5)
-p6 = RooRealVar("p6", "p6", -0.05, -0.001)
-
-a = ROOT.RooRealVar("a", "a", 1)
-b = ROOT.RooRealVar("b", "b", 2, -10, 10)
-y = ROOT.RooRealVar("y", "y", -10, 10)
-
-# ROOT.RooClassFactory.makePdf("super_novosibirsk", "x,p0,p1,p2,p3,p4,p5,p6")
 print("Compiling")
+w = RooWorkspace("w")
 ROOT.gROOT.ProcessLineSync(".x super_novosibirsk.cxx+")
 
-# super_novosibirisk_roo = RooGenericPdf(
-#     "super_novosibirisk_roo",
-#     "(0.5*(1+TMath::Erf((Mass - p0)/p1)))*1",
-#     RooArgList(Mass, p0, p1))
-print("Getting func")
-super_novosibirsk = ROOT.super_novosibirsk(
-    "super_novosibirsk", "super_novosibirsk", Mass, p0, p1, p3, p4, p5, p6)
-
 # frames = [Mass.frame() for i in range(0, len(correction_level)*len(lep))]
-frame = Mass.frame()
-print("Retrieving data from tree")
-data = RooDataSet("Mass", "Mass", tree, RooArgSet(Mass))
-data.plotOn(frame, Binning(1000))
-# input("Intermediate step: ")
-print("Plotting and fitting stuff")
-# super_novosibirsk.plotOn(frames[i])
 
-fitResult = super_novosibirsk.fitTo(
-    data, ROOT.RooFit.Save(), ROOT.RooFit.PrintLevel(-1))
-# fitResult.plotOn(frame)
-super_novosibirsk.plotOn(frame)
-frame.Draw()
+for c in correction_level_bkg:
+    print("Correction level: " + c)
+    ch = TChain("output_tree")
+    for e in eras:
+        ch.Add(os.path.join(input_dir_bkg, "_".join(["bkg", e, c]) + ".root"))
+    tree = ch.CopyTree("Leptonic_event")
+    Mass = RooRealVar("Mass", "Mass", 120, 600)
+    p0 = RooRealVar("p0", "p0", 0, 0.005)
+    p1 = RooRealVar("p1", "p1", 1600, 2000)
+    p3 = RooRealVar("p3", "p3", 30, 50)
+    p4 = RooRealVar("p4", "p4", 40, 80)
+    p5 = RooRealVar("p5", "p5", 0, 5)
+    p6 = RooRealVar("p6", "p6", -0.05, -0.001)
+    
+
+    print("Getting func")
+    super_novosibirsk = ROOT.super_novosibirsk(
+        "super_novosibirsk", "super_novosibirsk", Mass, p0, p1, p3, p4, p5, p6)
+    frame = Mass.frame()
+    print("Retrieving data from tree")
+    data = RooDataSet("Mass", "Mass", tree, RooArgSet(Mass))
+    data.plotOn(frame, Binning(100))
+    print("Plotting and fitting stuff")
+    # super_novosibirsk.plotOn(frames[i])
+    # fitResult = super_novosibirsk.fitTo(
+    #     data, ROOT.RooFit.Save(), ROOT.RooFit.PrintLevel(-1))
+    # fitResult.plotOn(frame)
+    super_novosibirsk.plotOn(frame)
+
+    # Now i need to do some tricks because some idiot decided to call a method
+    # as a python builtin.
+    getattr(w, 'import')(super_novosibirsk)
+    w.Print()
+    output_filename = c
+    output_filename = os.path.join(output_dir_fitted, c + ".root")
+    # out_file = TFile(os.path.join(output_dir_fitted, c + ".root"), "recreate")
+    w.writeToFile(output_filename, True)
+    # out_file.Close()
+    frame.Draw()
 
 input("asd")
